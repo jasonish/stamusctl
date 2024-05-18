19a9c73ba34f863eff21f79bc3e5cdee834172d3
bternoir <bternoir@stamus-networks.com>
2024-05-18 10:42:34 +0200

    fix:get interfaces without docker

--- internal/models/choices.go
+++ internal/models/choices.go
@@ -1,11 +1,8 @@
 package models
 
 import (
-	"strings"
-	"unicode"
-
-	"stamus-ctl/internal/docker"
-	"stamus-ctl/internal/logging"
+	"io/ioutil"
+	"log"
 )
 
 func GetChoices(name string) ([]Variable, error) {
@@ -30,37 +27,19 @@ func GetChoices(name string) ([]Variable, error) {
 }
 
 func getInterfaces() ([]Variable, error) {
-	s := logging.NewSpinner(
-		"Identifying interfaces",
-		"Did identify interfaces\n",
-	)
+	// Define the directory where network interfaces are listed
+	netDir := "/sys/class/net"
 
-	_, err := docker.PullImageIfNotExisted("busybox")
+	// Read the directory contents
+	files, err := ioutil.ReadDir(netDir)
 	if err != nil {
-		logging.SpinnerStop(s)
-		return nil, err
+		log.Fatalf("Failed to read directory %s: %v", netDir, err)
 	}
 
-	output, _ := docker.RunContainer("busybox", []string{
-		"ls",
-		"/sys/class/net",
-	}, nil, "host")
-
-	interfaces := strings.Split(output, "\n")
-	interfaces = interfaces[:len(interfaces)-1]
-	for i, in := range interfaces {
-		in = strings.TrimFunc(in, unicode.IsControl)
-		interfaces[i] = in
+	// Loop through the files and print the names
+	interfaces := []Variable{}
+	for _, file := range files {
+		interfaces = append(interfaces, CreateVariableString(file.Name()))
 	}
-	logging.Sugar.Debugw("detected interfaces.", "interfaces", interfaces)
-
-	interfacesVariables := []Variable{}
-	for _, in := range interfaces {
-		if in != "" {
-			interfacesVariables = append(interfacesVariables, CreateVariableString(in))
-		}
-	}
-
-	logging.SpinnerStop(s)
-	return interfacesVariables, nil
+	return interfaces, nil
 }
