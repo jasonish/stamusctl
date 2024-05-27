db63123837079370c6eee9ed6dbd348ec23337d2
bternoir <bternoir@stamus-networks.com>
2024-05-27 09:04:48 +0200

    feat:clean internal

--- internal/docker-compose/main.go
+++ internal/docker-compose/main.go
@@ -1,13 +1,5 @@
 package compose
 
-import (
-	"fmt"
-
-	"stamus-ctl/internal/logging"
-
-	"github.com/spf13/cobra"
-)
-
 func isValideRestartModeDocker(restart string) bool {
 	if restart != "no" &&
 		restart != "always" &&
@@ -20,40 +12,4 @@ func isValideRestartModeDocker(restart string) bool {
 
 func isValideRamSizeDocker(ram string) bool {
 	return ram != "" && ram != "0" && ram != "0m" && ram != "0g	" && ram != "0k" && ram != "0t" && ram != "0p"
-}
-
-func ValidateInputFlag(params Parameters) error {
-	if !isValideRestartModeDocker(params.RestartMode) {
-		return fmt.Errorf("please provid a valid value for --restart. %s is not valid", params.RestartMode)
-	}
-	if !isValideRamSizeDocker(params.ElasticMemory) {
-		return fmt.Errorf("please provide a valid value for --es-memory")
-	}
-	if !isValideRamSizeDocker(params.LogstashMemory) {
-		return fmt.Errorf("please provide a valid value for --ls-memory")
-	}
-	return nil
-}
-
-func GenerateComposeFileFromCli(cmd *cobra.Command, params *Parameters, nonInteractive bool) string {
-	if _, err := CheckVersions(); err != nil {
-		logging.Sugar.Fatal(err.Error())
-	}
-
-	if !nonInteractive {
-		Ask(cmd, params)
-	}
-
-	if params.InterfacesList == "" {
-		logging.Sugar.Fatal("please provide a valid network interface.")
-	}
-
-	if params.DebugMode {
-		params.NginxExec = "nginx"
-	} else {
-		params.NginxExec = "nginx-debug"
-	}
-	manifest := GenerateComposeFile(*params)
-
-	return manifest
 }
