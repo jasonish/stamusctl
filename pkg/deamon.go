cbda96ecbdb621c2b5b6d0e3a4c23d857fdc87f2
bternoir <bternoir@stamus-networks.com>
2024-07-30 17:03:33 +0200

    feat(deamon):unified pkg

--- pkg/deamon.go
+++ pkg/deamon.go
@@ -2,10 +2,24 @@ package pkg
 
 import "github.com/docker/docker/api/types"
 
+// Common
+type Config struct {
+	Value string `json:"config"` // Config name, default is tmp
+}
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+type SuccessResponse struct {
+	Message string `json:"message"`
+}
+type Containers struct {
+	Containers []string `json:"containers"`
+}
+
 // Init
 type InitRequest struct {
+	Config
 	IsDefault  bool              `json:"default"`     // Use default settings, default is false
-	Folder     string            `json:"folder"`      // Folder where to save configuration files, default is "tmp"
 	Project    string            `json:"project"`     // Project name, default is "selks"
 	Values     map[string]string `json:"values"`      // Values to set, key is the name of the value, value is the value
 	Version    string            `json:"version"`     // Target version, default is latest
@@ -15,29 +29,45 @@ type InitRequest struct {
 
 // Config
 type SetRequest struct {
+	Config
 	Reload     bool              `json:"reload"`      // Reload the configuration, don't keep arbitrary parameters
-	Project    string            `json:"project"`     // Project name, default is "tmp"
 	Values     map[string]string `json:"values"`      // Values to set, key is the name of the value, value is the value
 	Apply      bool              `json:"apply"`       // Apply the new configuration, relaunch it, default is false
 	ValuesPath string            `json:"values_path"` // Path to a values.yaml file
 	FromFile   map[string]string `json:"from_file"`   // Values keys and paths to files containing the content used as value
 }
 type GetRequest struct {
-	Project string   `json:"project"` // Project name, default is "tmp"
+	Config
 	Values  []string `json:"values"`  // Values to retrieve, default is all
 	Content bool     `json:"content"` // Get content or values, default is false
 }
 
 // Update
 type UpdateRequest struct {
+	Config
 	Version string            `json:"version"` // Version to update to, default is latest
-	Project string            `json:"project"` // Project name, default is tmp
 	Values  map[string]string `json:"values"`  // Values to set, key is the name of the value, value is the value
 }
 
+// Log
+type LogsRequest struct {
+	Containers []string `json:"containers"` // Containers ids to show logs from, default is all
+	Timestamps bool     `json:"timestamps"` // Show timestamps, default is false
+	Tail       string   `json:"tail"`       // Number of lines to show from the end, default is all
+	Since      string   `json:"since"`      // Show logs since (e.g. 2013-01-02T13:23:37Z) or relative (e.g. 42m for 42 minutes)
+	Until      string   `json:"until"`      // Show logs until(e.g. 2013-01-02T13:23:37Z) or relative (e.g. 42m for 42 minutes)
+}
+type ContainerLogs struct {
+	types.Container
+	Logs []string `json:"logs"` // Logs from the container
+}
+type LogsResponse struct {
+	Containers []ContainerLogs `json:"containers"`
+}
+
 // Wrapper
-type UpRequest struct {
-	Project string `json:"project"` // Project name, default is tmp
+type RestartRequest struct {
+	Containers []string `json:"containers"` // Container ID to restart,
 }
 type PsResponse struct {
 	Containers []types.Container `json:"containers"`
