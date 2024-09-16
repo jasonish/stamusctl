a919478ad2522eab8ed401709a16570bf8e79d4d
bternoir <bternoir@stamus-networks.com>
2024-09-16 12:00:08 +0200

    fix(models):correct file checks

--- internal/models/file.go
+++ internal/models/file.go
@@ -22,44 +22,47 @@ func CreateFileInstanceFromPath(path string) (File, error) {
 	// Extract the file properties
 	pathSplited := strings.Split(path, "/")
 	nameSplited := strings.Split(pathSplited[len(pathSplited)-1], ".")
-	// Validate all
+	// Validate name
 	if len(nameSplited) < 2 {
 		return File{}, fmt.Errorf("path %s is not a valid file name", path)
 	}
-	err := isValidPath(path)
+	// File
+	file := File{
+		Path: strings.Join(pathSplited[:len(pathSplited)-1], "/"),
+		Name: strings.Join(nameSplited[:len(nameSplited)-1], "."),
+		Type: nameSplited[len(nameSplited)-1],
+	}
+	// Validate all
+	err := file.isValidPath()
 	if err != nil {
 		return File{}, err
 	}
 	// Return file instance
-	return File{
-		Path: strings.Join(pathSplited[:len(pathSplited)-1], "/"),
-		Name: strings.Join(nameSplited[:len(nameSplited)-1], "."),
-		Type: nameSplited[len(nameSplited)-1],
-	}, nil
+	return file, nil
 }
 
 // Used create a file from path and name
 func CreateFileInstance(path string, fileName string) (File, error) {
 	// Extract the file properties
 	nameSplited := strings.Split(fileName, ".")
-	// Validate all
 	if len(nameSplited) != 2 {
 		return File{}, fmt.Errorf("path %s is not a valid file name", path)
 	}
-	err := isValidPath(path)
-	if err != nil {
-		return File{}, err
-	}
-	err = isValidPath(fileName)
-	if err != nil {
-		return File{}, err
-	}
-	// Return file instance
-	return File{
+	// File
+	file := File{
 		Path: path,
 		Name: nameSplited[0],
 		Type: nameSplited[1],
-	}, nil
+	}
+
+	// Validate all
+	err := file.isValidPath()
+	if err != nil {
+		return File{}, err
+	}
+
+	// Return file instance
+	return file, nil
 }
 
 func (f *File) completePath() string {
@@ -67,17 +70,20 @@ func (f *File) completePath() string {
 }
 
 // Empirical function to check if a path is valid
-func isValidPath(path string) error {
+func (f *File) isValidPath() error {
 	// Check if file already exists
-	if _, err := os.Stat(path); err == nil {
+	if _, err := os.Stat(f.completePath()); err == nil {
 		return nil
 	}
 	// Attempt to create it
 	var d []byte
-	if err := ioutil.WriteFile(path, d, 0644); err == nil {
-		os.Remove(path) // And delete it
+	if err := os.MkdirAll(f.Path, 0644); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(f.completePath(), d, 0644); err == nil {
+		os.Remove(f.completePath()) // And delete it
 		return nil
 	}
 	// Return error if not possible
-	return fmt.Errorf("path %s is not valid", path)
+	return fmt.Errorf("path %s is not valid", f.Path)
 }
