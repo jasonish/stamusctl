73829cdea7a24ff7c31eff1ba476f998f6f2810a
lanath <vvivier@stamus-networks.com>
2024-10-28 14:55:43 +0100

    feat(stamusctl): using docker run to read pcap

--- internal/handlers/compose/readPcap.go
+++ internal/handlers/compose/readPcap.go
@@ -2,14 +2,21 @@ package handlers
 
 import (
 	// Core
+	"bytes"
+	"context"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
+	"runtime/debug"
 	"strings"
 
 	// Internal
-	"stamus-ctl/internal/models"
+
+	"stamus-ctl/internal/logging"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
 )
 
 type ReadPcapParams struct {
@@ -17,62 +24,125 @@ type ReadPcapParams struct {
 	PcapPath string
 }
 
-func PcapHandler(params ReadPcapParams) error {
-	// Load existing config
-	confFile, err := models.CreateFileInstance(params.Config, "values.yaml")
+func initCli() *client.Client {
+	docker, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	cli := docker
+
 	if err != nil {
-		return err
-	}
-	existingConfig, err := models.LoadConfigFrom(confFile, false)
-	if err != nil {
-		return err
+		debug.PrintStack()
+		panic(err)
 	}
 
-	// Get existing config value
-	value, ok := existingConfig.GetParams().GetVariablesValues()["suricata.pcapreplay.hostpath"]
-	if !ok {
-		return fmt.Errorf("pcap_path parameter not found in existing config")
-	}
-	asString := value.AsString()
-
-	// Copy pcap file to the host path
-	splitted := strings.Split(params.PcapPath, "/")
-	err = CopyFile(params.PcapPath, filepath.Join(params.Config, asString, splitted[len(splitted)-1]))
-	if err != nil {
-		return err
-	}
-	fmt.Println("Pcap file is being read by the suricata container")
-
-	return nil
+	return cli
 }
 
-func CopyFile(src, dst string) error {
-	// Open source file
-	sourceFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
+func createConfig(configName, pcap string) (container.Config, container.HostConfig, network.NetworkingConfig, error) {
+	splitted := strings.Split(pcap, "/")
+	pcapName := splitted[len(splitted)-1]
 
-	// Create destination directory if it doesn't exist
-	destDir := filepath.Dir(dst)
-	err = os.MkdirAll(destDir, os.ModePerm)
+	dir, err := os.Getwd()
 	if err != nil {
-		return err
+
+		return container.Config{}, container.HostConfig{}, network.NetworkingConfig{}, nil
 	}
 
-	// Create destination file
-	destinationFile, err := os.Create(dst)
-	if err != nil {
-		return err
+	config := container.Config{
+		Image:      "jasonish/suricata:master-amd64-profiling",
+		Entrypoint: []string{"/new_entrypoint.sh"},
+		Cmd:        []string{"-k none -r /replay/" + pcapName + " --runmode autofp -l /var/log/suricata --set sensor-name=" + pcapName},
 	}
-	defer destinationFile.Close()
 
-	// Copy the contents of the source file to the destination file
-	_, err = io.Copy(destinationFile, sourceFile)
-	if err != nil {
-		return err
+	hostConfig := container.HostConfig{
+		AutoRemove: true,
+		Mounts: []mount.Mount{
+			{
+				Type:   mount.TypeBind,
+				Source: dir + "/" + configName + "/containers-data/suricata/etc",
+				Target: "/etc/suricata",
+			},
+			{
+				Type:   mount.TypeBind,
+				Source: dir + "/" + configName + "/containers-data/suricata/logs",
+				Target: "/var/log/suricata",
+			},
+			{
+				Type:     mount.TypeBind,
+				Source:   pcap,
+				Target:   "/replay/" + pcapName,
+				ReadOnly: true,
+			},
+			{
+				Type:     mount.TypeBind,
+				Source:   dir + "/" + configName + "/configs/suricata/new_entrypoint.sh",
+				Target:   "/new_entrypoint.sh",
+				ReadOnly: true,
+			},
+			{
+				Type:     mount.TypeBind,
+				Source:   dir + "/" + configName + "/configs/suricata/selks6-addin.yaml",
+				Target:   "/etc/suricata-configs/selks6-addin.yaml",
+				ReadOnly: true,
+			},
+		},
+		CapAdd: []string{"net_admin", "sys_nice"},
 	}
+
+	var networkConfig network.NetworkingConfig
+	return config, hostConfig, networkConfig, nil
+}
+
+func runContainer(configName, pcap string) (string, error) {
+	logger := logging.Sugar.With("name", "suricata-readpcap")
+	cli := initCli()
+	ctx := context.Background()
+	config, hostConfig, networkConfig, err := createConfig(configName, pcap)
+
+	if err != nil {
+		logger.With("error", err).Error("container configs")
+		return "", err
+	}
+
+	resp, err := cli.ContainerCreate(ctx, &config, &hostConfig, &networkConfig, nil, "")
+	if err != nil {
+		logger.With("error", err).Error("container create")
+		return "", err
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		logger.With("error", err).Error("container start")
+		return "", err
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			logger.With("error", err).Error("container wait")
+			return "", err
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true})
+	if err != nil {
+		logger.With("error", err).Error("container logs")
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(out)
+
+	output := buf.String()
+
+	logger.Debugw("run output", "output", output)
+
+	return output, nil
+}
+
+func PcapHandler(params ReadPcapParams) error {
+	output, _ := runContainer(params.Config, params.PcapPath)
+
+	fmt.Println(output)
 
 	return nil
 }
